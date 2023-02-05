e2a6343f5ef8788046a0d66b23c8843f4f3ec178
Alex JimeneZ <est.aherreraj0201>
2023-02-05 12:24:01 -0600

    Uptodate

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+//Add env variable to check if is production or development
+
+const env = process.env;
+//Set a base route if deploying on github pages
+env.mode = env.mode || "production";
+
+console.log('Environment: ', env.mode); 
+
+export default defineConfig({
+  root: "./",
+  //Add base route if deployiing on production
+  base: env.mode === "production" ? "/calificaciones-trimestrales/" : "/",
+  build: {
+    outDir: "docs",
+    assetsDir: "assets"
+  },
+  plugins: []
+});
